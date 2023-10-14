34ce15fa8b8d5c1e05a452e6485e41a3493eb0d9
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-10-14 04:53:13 -0700

    form added

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,100 @@
+
+
+const AddCoffee = () => {
+
+    const handleAddCoffee = e => {
+        e.preventDefault()
+        const form = e.target;
+        const name = form.name.value;
+        const quantity = form.quantity.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+
+        const newCoffee = {name, quantity, supplier, taste, category, details, photo}
+        console.log(newCoffee);
+    }
+    return (
+        <div className="bg-[#F4F3F0] p-24">
+            <h1>Add Coffee</h1>
+
+            <form onSubmit={handleAddCoffee}>
+                {/* form row  */}
+                <div className="md:flex gap-5">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Coffee Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="name" placeholder="Coffee Name" className="input input-bordered md:w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Coffee Quantity</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="quantity" placeholder="Coffee Quantity" className="input input-bordered md:w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form row  */}
+                <div className="md:flex gap-5">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Supplier</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="supplier" placeholder="Cappu Authorizer" className="input input-bordered md:w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Taste</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="taste" placeholder="Sweet and hot" className="input input-bordered md:w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form row  */}
+                <div className="md:flex gap-5">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Category</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="category" placeholder="Americano" className="input input-bordered md:w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Details</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="details" placeholder="Espresso with hot water" className="input input-bordered md:w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form row  */}
+                <div className="md:flex gap-5">
+                    <div className="form-control md:w-full">
+                        <label className="label">
+                            <span className="label-text">Photo</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="photo" placeholder="photoURL" className="input input-bordered md:w-full" />
+                        </label>
+                    </div>
+                </div>
+
+                <input className="btn btn-block bg-[#D2B48C] text-[#331A15] mt-10" type="submit" value="Add Coffee" />
+                 
+            </form>
+        </div>
+    );
+};
+
+export default AddCoffee;
